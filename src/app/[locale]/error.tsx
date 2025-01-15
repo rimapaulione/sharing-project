df87f560c227ecf5b42d073ee60753f5d60ba280
rimapaulione <rimapaulione@gmail.com>
2025-01-15 14:48:53 +0200

    add sentry and error pages

---
+++ src/app/[locale]/error.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import * as Sentry from "@sentry/nextjs";
+import { useTranslations } from "next-intl";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const t = useTranslations("ErrorPage");
+
+  useEffect(() => {
+    console.error(error);
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <div className="flex-none w-fit justify-self-center text-center">
+      <h2>{t("error")}</h2>
+      <Button className="m-10" size="sm" onClick={() => reset()}>
+        {t("errorButton")}
+      </Button>
+    </div>
+  );
+}
